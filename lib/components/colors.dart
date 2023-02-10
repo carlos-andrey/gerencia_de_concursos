25a31d2b9f9f695613a20047a62724fa30529a89
Andrey Alves <carlos.aas2001@gmail.com>
2023-02-10 13:38:51 -0300

    criando cores personalizadas

--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -4,3 +4,4 @@ const cor_aprovado = Color.fromRGBO(0, 255, 0, 1);
 const cor_reprovado = Color.fromRGBO(255, 0, 0, 1);
 const cor_azul_escuro = Color.fromRGBO(0, 59, 111, 1);
 const cor_azul = Color.fromRGBO(33, 150, 243, 1);
+const cor_preta = Color.fromRGBO(0, 0, 0, 1);
